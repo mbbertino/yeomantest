da271f4282a870a4c6794dc6880ab9c49822cb80
Matt Bertino <mattbertino@gmail.com>
2014-02-05 23:37:23 -0500

    commit with all the tests corrected

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,50 +1,50 @@
-// function Collection (models) {
-// 	this.models = models;
+function Collection (models) {
+	this.models = models;
 
-// 	this.find = function(id) {
-// 		var student;	
+	this.find = function(id) {
+		var student;	
 
-// 		this.models.forEach(function(value, index){
-// 			if(value.id == id) {
-// 				student = value;
-// 			}
-// 		});
+		this.models.forEach(function(value, index){
+			if(value.id == id) {
+				student = value;
+			}
+		});
 
-// 		if (student) {
-// 			return student;
-// 		};
-// 	}
+		if (student) {
+			return student;
+		};
+	}
 
-// 	this.add = function(newstudent) {
-// 		var result;
+	this.add = function(newstudent) {
+		var result;
 
-// 		if (newstudent.id == null || newstudent.name == null) {
-//       throw new Error("Whoops!");
-//     }
+		if (newstudent.id == null || newstudent.name == null) {
+      throw new Error("Whoops!");
+    }
 
-// 		this.models.push(newstudent);
-// 		result = newstudent;
-// 		return result;
-// 	}
+		this.models.push(newstudent);
+		result = newstudent;
+		return result;
+	}
 
-// 	this.remove = function(id) {
-// 		var result;
-// 		var success;
+	this.remove = function(id) {
+		var result;
+		var success;
 
-// 		if (typeof(id) != 'string') {
-//       throw new Error("Whoops!");
-//     }
+		if (typeof(id) != 'string') {
+      throw new Error("Whoops!");
+    }
 
-// 		this.models.forEach(function(value, index){
-// 			if(value.id == id) {
-// 				result = value;
-// 			}
-// 		});
+		this.models.forEach(function(value, index){
+			if(value.id == id) {
+				result = value;
+			}
+		});
 
-// 		var indx = this.models.indexOf(result);
-// 		this.models.splice(indx, 1)
-// 		success = true
-// 		return success
+		var indx = this.models.indexOf(result);
+		this.models.splice(indx, 1)
+		success = true
+		return success
 
-// 	}
-// }
+	}
+}
