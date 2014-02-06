b996cc5eafd0c4180f79ed62a2b6ade968025219
Matt Bertino <mattbertino@gmail.com>
2014-02-05 23:36:10 -0500

    commit with all tests failed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,50 @@
-function Collection (models) {
-	this.models = models
-}
+// function Collection (models) {
+// 	this.models = models;
 
+// 	this.find = function(id) {
+// 		var student;	
+
+// 		this.models.forEach(function(value, index){
+// 			if(value.id == id) {
+// 				student = value;
+// 			}
+// 		});
+
+// 		if (student) {
+// 			return student;
+// 		};
+// 	}
+
+// 	this.add = function(newstudent) {
+// 		var result;
+
+// 		if (newstudent.id == null || newstudent.name == null) {
+//       throw new Error("Whoops!");
+//     }
+
+// 		this.models.push(newstudent);
+// 		result = newstudent;
+// 		return result;
+// 	}
+
+// 	this.remove = function(id) {
+// 		var result;
+// 		var success;
+
+// 		if (typeof(id) != 'string') {
+//       throw new Error("Whoops!");
+//     }
+
+// 		this.models.forEach(function(value, index){
+// 			if(value.id == id) {
+// 				result = value;
+// 			}
+// 		});
+
+// 		var indx = this.models.indexOf(result);
+// 		this.models.splice(indx, 1)
+// 		success = true
+// 		return success
+
+// 	}
+// }
--- test/spec/test.js
+++ test/spec/test.js
@@ -2,35 +2,104 @@
 // should look at the chai library
 (function () {
     'use strict';
-
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-
-            it('should create a new instance', function () {
-              var students = new Collection;
-
-              expect(students.constructor.name).to.equal("Collection")
-            });
-
-            it('stores its first param in a prop called models', function() {
+    describe('A Collection instance', function () {
+    	describe('has a .find() method', function() {		
+            it('should return an object when given an id', function() {
             	var toBeAdded = [{name: 'Bower', id: '1'},{name: 'Jack', id: '2'}];
             	var students = new Collection(toBeAdded)
 
-            	expect(students.models).to.be.a('Array');
-            	expect(students.models[1].name).to.equal('Jack');
-           	});
-           	// homework below
-           	
-           	// end of homework
+            	expect(students.find('1')).to.eql({name: 'Bower', id: '1'});
+            });
+            it('should not return if there is not a matching id', function(){
+                var toBeAdded = [{name: 'Bower', id: '1'},{name: 'Jack', id: '2'}];
+                var students = new Collection(toBeAdded)
+
+                expect(students.find('100')).to.equal(undefined);
+                
+            });
+            it('should find an object with name and id as properties', function(){
+                var toBeAdded = [{name: 'Bower', id: '1'},{name: 'Jack', id: '2'}];
+                var students = new Collection(toBeAdded)
+
+                expect(students.find('1')).to.have.keys(['name', 'id']);
+                
+            });
+            it('should not equal a student with different id', function(){
+                var toBeAdded = [{name: 'Bower', id: '1'},{name: 'Jack', id: '2'}];
+                var students = new Collection(toBeAdded)
+
+                expect(students.find('1')).to.not.equal({name: 'Jack', id: '2'});
+                
+            });
         });
+        describe('has an .add() method', function() {
+            it("should add the object it's given to the models property", function(){
+                var toBeAdded = [{name: 'Bower', id: '1'},{name: 'Jack', id: '2'}];
+                var students = new Collection(toBeAdded);
+
+                expect(students.add({name: 'Jimmy', id: '3'})).to.eql({name: 'Jimmy', id: '3'})
+            });
+            it("should increase the length of the models database by 1", function(){
+                var toBeAdded = [{name: 'Bower', id: '1'},{name: 'Jack', id: '2'}];
+                var students = new Collection(toBeAdded);
+                students.add({name: 'Jimmy', id: '3'})
+
+                expect(students.models.length).to.equal(3)
+            });
+            it("should throw an error when given an object without and id value", function(){
+                var toBeAdded = [{name: 'Bower', id: '1'},{name: 'Jack', id: '2'}];
+                var students = new Collection(toBeAdded);
+
+                expect(function(){students.add({name: 'Jimmy', id: null})}).to.throw(Error);
+            });
+            it("should throw an error when given an object without and name value", function(){
+                var toBeAdded = [{name: 'Bower', id: '1'},{name: 'Jack', id: '2'}];
+                var students = new Collection(toBeAdded);
+
+                expect(function(){students.add({name: null, id: '3'})}).to.throw(Error);
+            });
+
+        })
+        describe('has an .remove() method', function() {
+            it("should, when given an id, remove the corresponding object from the models property", function(){
+                var toBeAdded = [{name: 'Bower', id: '1'},{name: 'Jack', id: '2'},{name: 'Jimmy', id: '3'}];
+                var students = new Collection(toBeAdded);
+                students.remove('2')
+
+                expect(students.models).to.eql([{name: 'Bower', id: '1'},{name: 'Jimmy', id: '3'}])
+            });
+            it("should decrease the models property's length by 1", function(){
+                var toBeAdded = [{name: 'Bower', id: '1'},{name: 'Jack', id: '2'},{name: 'Jimmy', id: '3'}];
+                var students = new Collection(toBeAdded);
+                students.remove('2')
+
+                expect(students.models.length).to.equal(2)
+            });
+            it("should only accept a single string as an id argument", function(){
+                var toBeAdded = [{name: 'Bower', id: '1'},{name: 'Jack', id: '2'},{name: 'Jimmy', id: '3'}];
+                var students = new Collection(toBeAdded);
+
+                expect(function(){students.remove(1)}).to.throw(Error);
+                expect(function(){students.remove({})}).to.throw(Error);
+                expect(function(){students.remove([])}).to.throw(Error);
+            });
+
+            it("should return true on successful removal", function(){
+                var toBeAdded = [{name: 'Bower', id: '1'},{name: 'Jack', id: '2'},{name: 'Jimmy', id: '3'}];
+                var students = new Collection(toBeAdded);
+
+                expect(students.remove('2')).to.eql(true)
+            });
+
+        })
     });
 })();
 
 
 // 
-students.find('1')
-students.add({name: 'Jimmy', id: '3'})
-students.remove('3')
+// students.find('1')
+// students.add({name: 'Jimmy', id: '3'})
+// students.remove('3')
 
 // create a commit log to masons repo
 //4 expects per test above
